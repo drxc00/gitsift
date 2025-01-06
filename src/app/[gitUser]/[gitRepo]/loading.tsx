5b29c53dbafdaca8deae78a8970e881f1b619fc8
Neil Patrick Villanueva <neilpatz16@gmail.com>
2025-01-06 22:05:56 +0800

    feat: added countdown timer for improved user feedback during loading state

--- src/app/[gitUser]/[gitRepo]/loading.tsx
+++ src/app/[gitUser]/[gitRepo]/loading.tsx
@@ -1,10 +1,26 @@
+"use client";
+
 import { Loader } from "@/components/loader";
+import { useEffect, useState } from "react";
 
 export default function Loading() {
+
+    // Timeout for loading state
+    const [timeOutRemaining, setTimeOutRemaining] = useState(10);
+
+    // Decrement the timeout remaining every second
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimeOutRemaining(prev => prev - 1);
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div className="fixed inset-0 flex flex-col items-center justify-center gap-4 z-50 bg-background">
             <Loader />
             <p className="text-lg text-muted-foreground">Sifting Repository</p>
+            <p className="text-sm text-muted-foreground">{timeOutRemaining} seconds remaining before timeout</p>
         </div>
     );
 }
