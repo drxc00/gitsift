e92ecf429a4cc1ba469f887e1be287afae32fd35
Neil Patrick Villanueva <neilpatz16@gmail.com>
2025-01-08 18:03:01 +0800

    fix: enhance error display component with dark mode support and improved text visibility

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -7,10 +7,10 @@ interface ErrorDisplayProps {
 
 export function ErrorDisplay ({message, header}: ErrorDisplayProps) {
     return (
-        <div className="flex flex-col justify-center bg-red-50 items-center gap-2 max-w-screen-sm mx-auto mt-10 mb-10 border border-red-500 rounded-lg p-4">
+        <div className="flex flex-col justify-center bg-red-50 dark:bg-red-950 items-center gap-2 max-w-screen-sm mx-auto mt-10 mb-10 border border-red-500 rounded-lg p-4">
             <TriangleAlert className="w-14 h-14 text-red-500" />
             <h1 className="text-2xl font-semibold text-red-500">{header || "An Error Occurred"}</h1>
-            <p className="text-center max-w-md text-muted-foreground">{message}</p>
+            <p className="text-center max-w-md text-muted-foreground dark:text-muted-foreground">{message}</p>
         </div>
     );
 }
